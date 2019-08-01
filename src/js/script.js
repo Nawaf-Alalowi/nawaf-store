cf6a9e9e22e4897f654dda1242fa0271cca24445
Nullify <070Nooaf@gmail.com>
2019-08-02 02:43:50 +0300

    There Push

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-      
+
+$(document).ready(function() {
+
     $('[data-toggle="tooltip"]').tooltip();
+    
+    $('[data-add-to-cart]').click(function(e) {
+        $(".notify").append("<b>Appended text</b>");
+        e.stopPropagation();
+    });
 });
